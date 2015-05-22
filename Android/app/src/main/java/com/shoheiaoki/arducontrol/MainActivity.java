0e9eae10e93c850dc25938f925814c7e58be8c92
Shohei Aoki <shoaok@gmail.com>
2015-05-22 20:55:37 +0900

    add websocket and usb

--- Android/app/src/main/java/com/shoheiaoki/arducontrol/MainActivity.java
+++ Android/app/src/main/java/com/shoheiaoki/arducontrol/MainActivity.java
@@ -5,15 +5,24 @@ import android.content.Context;
 import android.hardware.usb.UsbManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
+import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -22,14 +31,60 @@ public class MainActivity extends Activity {
     UsbManager manager;
     UsbSerialDriver usb;
     WebSocketClient mWebSocketClient;
+    final static String TAG = "WakeLock1";
+    PowerManager powerManager;
+    ScrollView mScrollView;
+    TextView mTextView;
+    Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        initUsb();
-        webSocketConnection();
 
+        powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, TAG);
+        wakeLock.acquire();
+        mScrollView = (ScrollView) findViewById(R.id.mScrollView);
+        mTextView = (TextView) findViewById(R.id.mTextView);
+        mHandler = new Handler(this.getMainLooper());
+        showFunkyMessage();
+
+        webSocketConnection();
+        initUsb();
+        start_read_thread(); // シリアル通信を読むスレッドを起動
+
+    }
+
+    protected void showMessage(final String msg){
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mTextView.append(msg);
+            }
+        });
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        },100);
+    }
+
+    protected void showFunkyMessage(){
+        try {
+            Process process = Runtime.getRuntime().exec("logcat -d");
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            StringBuilder log=new StringBuilder();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                log.append(line);
+            }
+            showMessage(log.toString());
+        } catch (IOException e) {
+        }
     }
 
     protected void webSocketConnection() {
@@ -54,7 +109,7 @@ public class MainActivity extends Activity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Log.v("received: ",message);
+                        Log.v("hoge", "received: " + message);
                     }
                 });
             }
@@ -80,8 +135,7 @@ public class MainActivity extends Activity {
             try {
                 usb.open();
                 usb.setBaudRate(9600);
-                start_read_thread(); // シリアル通信を読むスレッドを起動
-                start_write_thread(); // シリアル通信を読むスレッドを起動
+//                start_write_thread(); // シリアル通信を読むスレッドを起動
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -95,8 +149,16 @@ public class MainActivity extends Activity {
                     while (true) {
                         byte buf[] = new byte[256];
                         int num = usb.read(buf, buf.length);
-                        if (num > 0)
-                            Log.v("arduino", new String(buf, 0, num)); // Arduinoから受信した値をlogcat出力
+                        if (num > 0) {
+                            String readMsg = new String(buf,0,num);
+                            Log.v("arduino", readMsg); // Arduinoから受信した値をlogcat出力
+                            showMessage(readMsg); // Arduinoから受信した値をlogcat出力
+                            try {
+                                mWebSocketClient.send(readMsg);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
                         Thread.sleep(10);
                     }
                 } catch (IOException e) {
